ab63f6e1e3c12ab17ba5583529c4a9af9c4d9ecb
Tamilore <findtamilore@gmail.com>
2023-11-16 12:07:48 +0100

    sub opcode correction

--- operationCode2.c
+++ operationCode2.c
@@ -57,7 +57,7 @@ void sub(stack_t **stack, unsigned int line_number)
 
 	if (!stack || !(*stack) || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
