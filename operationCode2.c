6b58c05aa31a1d35c56e425fa96954cdd9a92358
Tamilore <findtamilore@gmail.com>
2023-11-16 11:44:03 +0100

    add opcode update

--- operationCode2.c
+++ operationCode2.c
@@ -1,19 +1,21 @@
 #include "monty.h"
 
 /**
- * add - Adds the top two elements of the stack
- * @stack: Double pointer to the head of the stack
- * @linecount: Line number in the source file
- *
- * This function adds the top two elements of the stack and replaces them
- * with the result. If the stack contains less than two elements, it prints
- * an error message and exits with EXIT_FAILURE.
- *
- * Return: void
- */
+* add - Adds the top two elements of the stack
+* @stack: Double pointer to the head of the stack
+* @linecount: Line number in the source file
+*
+* This function adds the top two elements of the stack and replaces them
+* with the result. If the stack contains less than two elements, it prints
+* an error message and exits with EXIT_FAILURE.
+*
+* Return: void
+*/
 void add(stack_t **stack, unsigned int linecount)
 {
 	int result;
+
+	stack_t *temp = NULL;
 
 	if (!stack || !(*stack) || !(*stack)->next)
 	{
@@ -22,6 +24,8 @@ void add(stack_t **stack, unsigned int linecount)
 	}
 
 	result = (*stack)->n + (*stack)->next->n;
-
-	printf("%d\n", result);
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->n = result;
+	free(temp);
 }
