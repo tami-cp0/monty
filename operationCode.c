82d01d6fb3909eebc836585088023ba879833ca7
Tamilore <findtamilore@gmail.com>
2023-11-16 01:28:01 +0100

    updated swap

--- operationCode.c
+++ operationCode.c
@@ -136,7 +136,7 @@ void swap(stack_t **stack, unsigned int line_number)
 	stack_t *temp;
 	stack_t *current;
 
-	if ((*stack == NULL || (*stack)->next == NULL))
+	if ((*stack == NULL && (*stack)->next == NULL))
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short", line_number);
 		exit(EXIT_FAILURE);
