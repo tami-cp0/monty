bdb82f517722120a685cc968cf29e4825a284c1e
Tamilore <findtamilore@gmail.com>
2023-11-16 12:25:21 +0100

    div correction

--- operationCode2.c
+++ operationCode2.c
@@ -69,10 +69,12 @@ void sub(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * my_div - Divides the second top element by the top element
- * @stack: Double pointer to the head of the stack
- * @line_number: Line number in the source file
- */
+* my_div - Divides the second top element by the top element
+* @stack: Double pointer to the head of the stack
+* @line_number: Line number in the source file
+*
+* Return: Void
+*/
 void my_div(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = NULL;
@@ -81,6 +83,12 @@ void my_div(stack_t **stack, unsigned int line_number)
 	if (!stack || !(*stack) || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
